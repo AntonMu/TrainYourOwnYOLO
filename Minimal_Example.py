acf2e368ba8f1407c3e84d3fdba9149f49107aa8
AntonMu <antonmuehlemann@gmail.com>
2019-09-13 14:43:01 -0700

    Update Minimal_Example.py

--- Minimal_Example.py
+++ Minimal_Example.py
@@ -23,84 +23,27 @@ download_script = os.path.join(model_folder,'Download_Weights.py')
 
 print('Downloading Pretrained Weights')
 start = time.time()
-call_string = ' '.join(['python',download_script,'1aPCwYXFAOmklmNMLMh81Yduw5UrbHqkN', os.path.join(model_folder,'Houses','trained_weights_final.h5') ])
+call_string = ' '.join(['python',download_script,'1MGXAP_XD_w4OExPP10UHsejWrMww8Tu7', os.path.join(model_folder,'trained_weights_final.h5') ])
 
 subprocess.call(call_string , shell=True)
 
-call_string = ' '.join(['python',download_script, '1FbvHzQWCjucXPbTbI4S1MnBLkAi58Mxv', os.path.join(model_folder,'Openings','trained_weights_final.h5') ])
-
-subprocess.call(call_string , shell=True)
 end = time.time()
 print('Downloaded Pretrained Weights in {0:.1f} seconds'.format(end-start))
 
-# Now run the housing detector
-detector_script = os.path.join(os.path.dirname(os.path.abspath(__file__)),'2_Computer_Vision','detector.py')
+# Now run the cat face detector
+detector_script = os.path.join(os.path.dirname(os.path.abspath(__file__)),'3_Inference','Detector.py')
 
 
-houses_result_file =  os.path.join(test_folder, 'Housing_Results.csv')
-houses_weights = os.path.join(model_folder,'Houses','trained_weights_final.h5')
-houses_classes = os.path.join(model_folder,'Houses','data_classes.txt')
-anchors = os.path.join(root_folder,'2_Computer_Vision','src','keras_yolo3','model_data','yolo_anchors.txt')
+result_file =  os.path.join(output_folder, 'Cat_Faces_Results.csv')
+model_weights = os.path.join(model_folder,'trained_weights_final.h5')
+classes_file = os.path.join(model_folder,'data_classes.txt')
+anchors = os.path.join(root_folder,'2_Training','src','keras_yolo3','model_data','yolo_anchors.txt')
 
-arglist = [['input_images',street_view_folder],['classes',houses_classes],['output',test_folder],['yolo_model',houses_weights],['box_file',houses_result_file],['anchors',anchors]]
+arglist = [['input_images',input_folder],['classes',classes_file],['output',output_folder],['yolo_model',model_weights],['box_file',result_file],['anchors',anchors]]
 call_string = ' '.join(['python', detector_script,make_call_string(arglist)])
 
-print('Detecting Houses by calling \n\n', call_string,'\n')
+print('Detecting Cat Faces by calling \n\n', call_string,'\n')
 start = time.time()
 subprocess.call(call_string, shell=True)
 end = time.time()
-print('Detected Houses in {0:.1f} seconds'.format(end-start))
-
-# #Next, we crop out the houses
-cropping_script = os.path.join(root_folder,'2_Computer_Vision','Crop_Images.py')
-
-
-cropping_result_file = os.path.join(test_folder,'Cropping_Results.csv') 
-
-
-arglist = [['input_file',houses_result_file],['classes',houses_classes],['output_folder',cropped_folder],['output_file',cropping_result_file]]
-
-
-call_string = ' '.join(['python', cropping_script,make_call_string(arglist)])
-
-print('Cropping Houses by calling \n\n', call_string,'\n')
-start = time.time()
-subprocess.call(call_string, shell=True)
-end = time.time()
-print('Cropped Houses in {0:.1f} seconds'.format(end-start))
-
-
-# Next run the opening detector
-
-model_folder =  os.path.join(data_folder,'Model_Weights')
-opening_weights = os.path.join(model_folder,'Openings','trained_weights_final.h5')
-opening_classes = os.path.join(model_folder,'Openings','data_all_classes.txt')
-detector_script = os.path.join(root_folder,'2_Computer_Vision','detector.py')
-openings_result_file =  os.path.join(test_folder, 'Opening_Results.csv')
-
-arglist = [['input_images',cropped_folder],['classes',opening_classes],['output',test_folder],['yolo_model',opening_weights],['box_file',openings_result_file],['anchors',anchors],['postfix','_opening']]
-call_string = ' '.join(['python', detector_script,make_call_string(arglist)])
-
-print('Detecting Openings by calling \n\n', call_string,'\n')
-start = time.time()
-subprocess.call(call_string, shell=True)
-end = time.time()
-print('Detected Openings in {0:.1f} seconds'.format(end-start))
-
-#Finally run the classification
-
-classifier_script = os.path.join(root_folder,'3_Classification','Classifier.py')
-
-level_folder =  os.path.join(data_folder,'Level_Detection_Results')
-softness_score_file =  os.path.join(test_folder, 'Softness_Scores.csv')
-
-arglist = [['output_file',softness_score_file],['input_file',openings_result_file],['level_folder',test_folder],['classes',opening_classes]]
-
-call_string = ' '.join(['python', classifier_script,make_call_string(arglist)])
-
-
-print('Calculating Softness Scores by calling \n\n', call_string,'\n')
-start = time.time()
-subprocess.call(call_string, shell=True)
-end = time.time()
-print('Calculated Softness Scores in {0:.1f} seconds'.format(end-start))
+print('Detected Cat Faces in {0:.1f} seconds'.format(end-start))
