687512d3d411109557ec7b292676d5dae18a0ce8
AntonMu <antonmuehlemann@gmail.com>
2019-09-16 23:14:54 -0700

    Update Minimal_Example.py

--- Minimal_Example.py
+++ Minimal_Example.py
@@ -35,7 +35,7 @@ print('Downloaded Pretrained Weights in {0:.1f} seconds'.format(end-start))
 detector_script = os.path.join(os.path.dirname(os.path.abspath(__file__)),'3_Inference','Detector.py')
 
 
-result_file =  os.path.join(output_folder, 'Cat_Faces_Results.csv')
+result_file =  os.path.join(output_folder, 'Detection_Results.csv')
 model_weights = os.path.join(model_folder,'trained_weights_final.h5')
 classes_file = os.path.join(model_folder,'data_classes.txt')
 anchors = os.path.join(root_folder,'2_Training','src','keras_yolo3','model_data','yolo_anchors.txt')
