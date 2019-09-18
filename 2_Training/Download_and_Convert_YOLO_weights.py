94faa261ded5a4ce258a90e22a724c1936561704
AntonMu <muehle@berkeley.edu>
2019-09-17 23:14:56 -0700

    Update Download_and_Convert_YOLO_weights.py

--- 2_Training/Download_and_Convert_YOLO_weights.py
+++ 2_Training/Download_and_Convert_YOLO_weights.py
@@ -2,13 +2,37 @@ import os
 import subprocess
 import time
 import sys
+import argparse
+import requests
+
+FLAGS = None
 
 root_folder = os.path.dirname(os.path.abspath(__file__))
-
 download_folder = os.path.join(root_folder,'src','keras_yolo3')
 
-convert_script = os.path.join(download_folder,'Download_YOLO_weights.sh')
+# cd $(dirname $0)
+# wget https://pjreddie.com/media/files/yolov3.weights
+# python convert.py yolov3.cfg yolov3.weights model_data/yolo.h5
+if __name__ == '__main__':
+    # Delete all default flags
+    parser = argparse.ArgumentParser(argument_default=argparse.SUPPRESS)
+    '''
+    Command line options
+    '''
+    parser.add_argument(
+        "--download_folder", type=str, default=download_folder,
+        help = "Folder to download weights to. Default is "+ download_folder
+        )
 
-call_string = 'bash ' + convert_script
+    
+    FLAGS = parser.parse_args()
 
-subprocess.call(call_string , shell=True)
+    url = 'https://pjreddie.com/media/files/yolov3.weights'
+    r = requests.get(url)
+
+    with open(os.path.join(download_folder,'yolov3.weights'), 'wb') as f:
+        f.write(r.content)
+
+    call_string = 'python convert.py yolov3.cfg yolov3.weights model_data/yolo.h5'
+
+    subprocess.call(call_string , shell=True, cwd = download_folder )
