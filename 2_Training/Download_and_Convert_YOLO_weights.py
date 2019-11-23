a984225184fbc13c541f5db4f78d3f79df1050f5
Niklas Wilson <it@niklaswilson.net>
2019-11-23 10:11:09 -0600

    Updated Download&Convert to NOT redownload

--- 2_Training/Download_and_Convert_YOLO_weights.py
+++ 2_Training/Download_and_Convert_YOLO_weights.py
@@ -11,6 +11,30 @@ FLAGS = None
 root_folder = os.path.dirname(os.path.abspath(__file__))
 download_folder = os.path.join(root_folder,'src','keras_yolo3')
 
+def downloadIfNeeded(pathWithFileName, url):
+    needFile = True; f = None
+    try:
+        f = open(pathWithFileName)
+        needFile = False
+    except IOError: pass
+    finally:
+        if f is not None: f.close()
+    if needFile:
+        r = requests.get(url,stream=True)
+        f = open(pathWithFileName, 'wb')
+        file_size = int(r.headers.get('content-length'))
+        chunk = 100
+        num_bars = file_size // chunk
+        bar =  progressbar.ProgressBar(maxval=num_bars).start()
+        i = 0
+        for chunk in r.iter_content(chunk):
+            f.write(chunk)
+            bar.update(i)
+            i+=1
+        f.close()
+    else:
+        print("Already downloaded, skipping download, of file:", pathWithFileName)
+
 if __name__ == '__main__':
     # Delete all default flags
     parser = argparse.ArgumentParser(argument_default=argparse.SUPPRESS)
@@ -22,23 +46,12 @@ if __name__ == '__main__':
         help = "Folder to download weights to. Default is "+ download_folder
         )
 
-    
     FLAGS = parser.parse_args()
 
-    url = 'https://pjreddie.com/media/files/yolov3.weights'
-    r = requests.get(url,stream=True)
-
-    f = open(os.path.join(download_folder,'yolov3.weights'), 'wb')
-    file_size = int(r.headers.get('content-length'))
-    chunk = 100
-    num_bars = file_size // chunk
-    bar =  progressbar.ProgressBar(maxval=num_bars).start()
-    i = 0
-    for chunk in r.iter_content(chunk):
-        f.write(chunk)
-        bar.update(i)
-        i+=1
-    f.close()
+    downloadIfNeeded(
+        os.path.join(download_folder,'yolov3.weights'),
+        'https://pjreddie.com/media/files/yolov3.weights'
+    )
 
     call_string = 'python convert.py yolov3.cfg yolov3.weights yolo.h5'
 
