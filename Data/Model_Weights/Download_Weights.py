459253b393a3fd1001feabc1f7ef78764c9d9c58
AntonMu <antonmuehlemann@gmail.com>
2019-12-27 00:58:17 -0800

    Update README.md
    Apply black formatting
    
    remove pycache
    
    delete multiple gitignore
    
    allow white space in folder name

--- Data/Model_Weights/Download_Weights.py
+++ Data/Model_Weights/Download_Weights.py
@@ -1,19 +1,20 @@
-#Modified from https://stackoverflow.com/questions/25010369/wget-curl-large-file-from-google-drive
+# Modified from https://stackoverflow.com/questions/25010369/wget-curl-large-file-from-google-drive
 
-#To download houses weights run
+# To download houses weights run
 # python Download_Weights.py 1aPCwYXFAOmklmNMLMh81Yduw5UrbHqkN Houses/trained_weights_final.h5
 
-#To download openeings weights run
+# To download openeings weights run
 # python Download_Weights.py 1FbvHzQWCjucXPbTbI4S1MnBLkAi58Mxv Openings/trained_weights_final.h5
 
 import requests
 import os
 import progressbar
 
+
 def download_file_from_google_drive(id, destination):
     def get_confirm_token(response):
         for key, value in response.cookies.items():
-            if key.startswith('download_warning'):
+            if key.startswith("download_warning"):
                 return value
 
         return None
@@ -23,34 +24,35 @@ def download_file_from_google_drive(id, destination):
 
         with open(destination, "wb") as f:
             bar = progressbar.ProgressBar(max_value=progressbar.UnknownLength)
-            i=0
+            i = 0
             for chunk in response.iter_content(CHUNK_SIZE):
-                if chunk: # filter out keep-alive new chunks
+                if chunk:  # filter out keep-alive new chunks
                     bar.update(i)
-                    i+=1
+                    i += 1
                     f.write(chunk)
 
     URL = "https://docs.google.com/uc?export=download"
 
     session = requests.Session()
 
-    response = session.get(URL, params = { 'id' : id }, stream = True)
+    response = session.get(URL, params={"id": id}, stream=True)
     token = get_confirm_token(response)
 
     if token:
-        params = { 'id' : id, 'confirm' : token }
-        response = session.get(URL, params = params, stream = True)
+        params = {"id": id, "confirm": token}
+        response = session.get(URL, params=params, stream=True)
 
-    save_response_content(response, destination)    
+    save_response_content(response, destination)
 
 
 if __name__ == "__main__":
     import sys
+
     if len(sys.argv) is not 3:
         print("Usage: python google_drive.py drive_file_id destination_file_path")
     else:
         # TAKE ID FROM SHAREABLE LINK
         file_id = sys.argv[1]
         # DESTINATION FILE ON YOUR DISK
-        destination = os.path.join(os.getcwd(),sys.argv[2])
+        destination = os.path.join(os.getcwd(), sys.argv[2])
         download_file_from_google_drive(file_id, destination)
--- Utils/utils.py
+++ Utils/utils.py
@@ -11,10 +11,10 @@ from timeit import default_timer as timer
 # import readline
 # readline.parse_and_bind("tab: complete")
 
-min_logo_size = (10,10)
+min_logo_size = (10, 10)
 
 
-def detect_object(yolo, img_path, save_img, save_img_path='./', postfix=''):
+def detect_object(yolo, img_path, save_img, save_img_path="./", postfix=""):
     """
     Call YOLO logo detector on input image, optionally save resulting image.
 
@@ -34,7 +34,7 @@ def detect_object(yolo, img_path, save_img, save_img_path='./', postfix=''):
             image = image.convert("RGB")
         image_array = np.array(image)
     except:
-        print('File Open Error! Try again!')
+        print("File Open Error! Try again!")
         return None, None
 
     prediction, new_image = yolo.detect_image(image)
@@ -52,21 +52,25 @@ def parse_input():
     """
     out = []
     while True:
-        ins = input('Enter path (q to quit):').strip()
-        if ins in ['q','quit']:
+        ins = input("Enter path (q to quit):").strip()
+        if ins in ["q", "quit"]:
             break
         if not os.path.exists(ins):
-            print('Error: file not found!')
+            print("Error: file not found!")
         elif os.path.isdir(ins):
-            out = [ os.path.abspath(os.path.join(ins,f)) for f in os.listdir(ins) if f.endswith(('.jpg', '.png')) ]
+            out = [
+                os.path.abspath(os.path.join(ins, f))
+                for f in os.listdir(ins)
+                if f.endswith((".jpg", ".png"))
+            ]
             break
-        elif ins.endswith(('.jpg', '.png')):
+        elif ins.endswith((".jpg", ".png")):
             out.append(os.path.abspath(ins))
         print(out)
     return out
 
 
-def load_extractor_model(model_name='InceptionV3', flavor=1):
+def load_extractor_model(model_name="InceptionV3", flavor=1):
     """Load variant of InceptionV3 or VGG16 model specified.
 
     Args:
@@ -77,29 +81,33 @@ def load_extractor_model(model_name='InceptionV3', flavor=1):
         For VGG16, it only changes the input size, {0: 224 (default), 1: 128, 2: 64}.
 """
     start = timer()
-    if model_name == 'InceptionV3':
+    if model_name == "InceptionV3":
         from keras.applications.inception_v3 import InceptionV3
         from keras.applications.inception_v3 import preprocess_input
-        model = InceptionV3(weights='imagenet', include_top=False)
+
+        model = InceptionV3(weights="imagenet", include_top=False)
 
         trunc_layer = [-1, 279, 248, 228, -1]
         i_layer = flavor
-        model_out = Model(inputs=model.inputs, outputs=model.layers[trunc_layer[i_layer]].output)
-        input_shape = (299,299,3) if flavor==0 else (200,200,3)
+        model_out = Model(
+            inputs=model.inputs, outputs=model.layers[trunc_layer[i_layer]].output
+        )
+        input_shape = (299, 299, 3) if flavor == 0 else (200, 200, 3)
 
-    elif model_name == 'VGG16':
+    elif model_name == "VGG16":
         from keras.applications.vgg16 import VGG16
         from keras.applications.vgg16 import preprocess_input
-        model_out = VGG16(weights='imagenet', include_top=False)
-        input_length = [224,128,64][flavor]
-        input_shape = (input_length,input_length,3)
+
+        model_out = VGG16(weights="imagenet", include_top=False)
+        input_length = [224, 128, 64][flavor]
+        input_shape = (input_length, input_length, 3)
 
     end = timer()
-    print('Loaded {} feature extractor in {:.2f}sec'.format(model_name, end-start))
+    print("Loaded {} feature extractor in {:.2f}sec".format(model_name, end - start))
     return model_out, preprocess_input, input_shape
 
 
-def chunks(l, n, preprocessing_function = None):
+def chunks(l, n, preprocessing_function=None):
     """Yield successive n-sized chunks from l.
 
     General purpose function modified for Keras: made infinite loop,
@@ -118,7 +126,7 @@ def chunks(l, n, preprocessing_function = None):
     # in predict_generator, steps argument sets how many times looped through "while True"
     while True:
         for i in range(0, len(l), n):
-            yield np.array([func(el) for el in l[i:i + n]])
+            yield np.array([func(el) for el in l[i : i + n]])
 
 
 def load_features(filename):
@@ -128,36 +136,42 @@ def load_features(filename):
 
     start = timer()
     # get database features
-    with  h5py.File(filename, 'r') as hf:
-        brand_map = list(hf.get('brand_map'))
-        input_shape = list(hf.get('input_shape'))
-        features = hf.get('features')
+    with h5py.File(filename, "r") as hf:
+        brand_map = list(hf.get("brand_map"))
+        input_shape = list(hf.get("input_shape"))
+        features = hf.get("features")
         features = np.array(features)
     end = timer()
-    print('Loaded {} features from {} in {:.2f}sec'.format(features.shape, filename, end-start))
+    print(
+        "Loaded {} features from {} in {:.2f}sec".format(
+            features.shape, filename, end - start
+        )
+    )
 
     return features, brand_map, input_shape
+
 
 def save_features(filename, features, brand_map, input_shape):
     """
     Save features to compressed HDF5 file for later use
     """
 
-    print('Saving {} features into {}... '.format(features.shape, filename), end='')
+    print("Saving {} features into {}... ".format(features.shape, filename), end="")
     # reduce file size by saving as float16
     features = features.astype(np.float16)
     start = timer()
-    with h5py.File(filename, 'w') as hf:
-        hf.create_dataset('features', data = features, compression='lzf')
-        hf.create_dataset('brand_map', data = brand_map)
-        hf.create_dataset('input_shape', data = input_shape)
+    with h5py.File(filename, "w") as hf:
+        hf.create_dataset("features", data=features, compression="lzf")
+        hf.create_dataset("brand_map", data=brand_map)
+        hf.create_dataset("input_shape", data=input_shape)
 
     end = timer()
-    print('done in {:.2f}sec'.format(end-start))
+    print("done in {:.2f}sec".format(end - start))
 
     return None
 
-def features_from_image(img_array, model, preprocess, batch_size = 100):
+
+def features_from_image(img_array, model, preprocess, batch_size=100):
     """
     Extract features from image array given a decapitated keras model.
     Use a generator to avoid running out of memory for large inputs.
@@ -172,12 +186,12 @@ def features_from_image(img_array, model, preprocess, batch_size = 100):
     if len(img_array) == 0:
         return np.array([])
 
-    steps = len(img_array)//batch_size + 1
-    img_gen = chunks(img_array, batch_size, preprocessing_function = preprocess)
-    features = model.predict_generator(img_gen, steps = steps)
+    steps = len(img_array) // batch_size + 1
+    img_gen = chunks(img_array, batch_size, preprocessing_function=preprocess)
+    features = model.predict_generator(img_gen, steps=steps)
 
     # if the generator has looped past end of array, cut it down
-    features = features[:len(img_array)]
+    features = features[: len(img_array)]
 
     # reshape features: flatten last three dimensions to one
     features = features.reshape(features.shape[0], np.prod(features.shape[1:]))
@@ -189,8 +203,7 @@ def features_from_image(img_array, model, preprocess, batch_size = 100):
 ##################################################
 
 
-
-def pad_image(img, shape, mode = 'constant_mean'):
+def pad_image(img, shape, mode="constant_mean"):
     """
     Resize and pad image to given size.
 
@@ -203,25 +216,25 @@ def pad_image(img, shape, mode = 'constant_mean'):
     Returns:
       new_im: (H', W', C) padded numpy array
     """
-    if mode == 'constant_mean':
-        mode_args = {'mode': 'constant', 'constant_values': np.mean(img)}
+    if mode == "constant_mean":
+        mode_args = {"mode": "constant", "constant_values": np.mean(img)}
     else:
-        mode_args = {'mode': mode}
+        mode_args = {"mode": mode}
 
     ih, iw = img.shape[:2]
     h, w = shape[:2]
 
     # first rescale image so that largest dimension matches target
-    scale = min(w/iw, h/ih)
+    scale = min(w / iw, h / ih)
     nw, nh = int(iw * scale), int(ih * scale)
     img = cv2.resize(img, (nw, nh))
 
     # center-pad rest of image: compute padding and split in two
-    xpad, ypad = shape[1]-nw, shape[0]-nh
-    xpad = (xpad//2, xpad//2+xpad%2)
-    ypad = (ypad//2, ypad//2+ypad%2)
+    xpad, ypad = shape[1] - nw, shape[0] - nh
+    xpad = (xpad // 2, xpad // 2 + xpad % 2)
+    ypad = (ypad // 2, ypad // 2 + ypad % 2)
 
-    new_im = np.pad(img, pad_width=(ypad, xpad, (0,0)), **mode_args)
+    new_im = np.pad(img, pad_width=(ypad, xpad, (0, 0)), **mode_args)
 
     return new_im
 
@@ -235,8 +248,8 @@ def bbox_colors(n):
     Returns:
       colors: (n, 3) np.array with RGB integer values in [0-255] range
     """
-    hsv_tuples = [(x / n, 1., 1.) for x in range(n)]
-    colors = 255 * np.array([ colorsys.hsv_to_rgb(*x) for x in hsv_tuples])
+    hsv_tuples = [(x / n, 1.0, 1.0) for x in range(n)]
+    colors = 255 * np.array([colorsys.hsv_to_rgb(*x) for x in hsv_tuples])
 
     np.random.seed(10101)  # Fixed seed for consistent colors across runs.
     np.random.shuffle(colors)  # Shuffle colors to decorrelate adjacent classes.
@@ -244,7 +257,8 @@ def bbox_colors(n):
 
     return colors.astype(int)
 
-def contents_of_bbox(img, bbox_list, expand=1.):
+
+def contents_of_bbox(img, bbox_list, expand=1.0):
     """
     Extract portions of image inside  bounding boxes list.
 
@@ -257,7 +271,7 @@ def contents_of_bbox(img, bbox_list, expand=1.):
       i_candidates_too_small: list of indices of small candidates dropped
     """
 
-    candidates =[]
+    candidates = []
     i_candidates_too_small = []
     for i, (xmin, ymin, xmax, ymax, *_) in enumerate(bbox_list):
 
@@ -265,11 +279,11 @@ def contents_of_bbox(img, bbox_list, expand=1.):
         if ymin > img.shape[0] or xmin > img.shape[1]:
             continue
 
-        xmin, ymin = int(xmin//expand), int(ymin//expand)
-        xmax, ymax = int(np.round(xmax//expand)), int(np.round(ymax//expand))
+        xmin, ymin = int(xmin // expand), int(ymin // expand)
+        xmax, ymax = int(np.round(xmax // expand)), int(np.round(ymax // expand))
 
         # do not even consider tiny logos
-        if xmax-xmin > min_logo_size[1] and ymax-ymin > min_logo_size[0]:
+        if xmax - xmin > min_logo_size[1] and ymax - ymin > min_logo_size[0]:
             candidates.append(img[ymin:ymax, xmin:xmax])
         else:
             i_candidates_too_small.append(i)
@@ -291,8 +305,12 @@ def draw_annotated_box(image, box_list_list, label_list, color_list):
       image: annotated PIL image object
     """
 
-    font_path = os.path.join(os.path.dirname(__file__), 'keras_yolo3/font/FiraMono-Medium.otf')
-    font = ImageFont.truetype(font = font_path, size=np.floor(3e-2 * image.size[1] + 0.5).astype('int32'))
+    font_path = os.path.join(
+        os.path.dirname(__file__), "keras_yolo3/font/FiraMono-Medium.otf"
+    )
+    font = ImageFont.truetype(
+        font=font_path, size=np.floor(3e-2 * image.size[1] + 0.5).astype("int32")
+    )
     thickness = (image.size[0] + image.size[1]) // 300
 
     draw = ImageDraw.Draw(image)
@@ -302,30 +320,31 @@ def draw_annotated_box(image, box_list_list, label_list, color_list):
             color = tuple(color)
         for box in box_list:
             # deal with empty predictions
-            if len(box)<4:
+            if len(box) < 4:
                 continue
 
             # if score is also passed, append to label
-            thelabel = '{}'.format(label)
-            if len(box)>4:
-                thelabel += ' {:.2f}'.format(box[-1])
+            thelabel = "{}".format(label)
+            if len(box) > 4:
+                thelabel += " {:.2f}".format(box[-1])
             label_size = draw.textsize(thelabel, font)
 
             xmin, ymin, xmax, ymax = box[:4]
-            ymin = max(0, np.floor(ymin + 0.5).astype('int32'))
-            xmin = max(0, np.floor(xmin + 0.5).astype('int32'))
-            ymax = min(image.size[1], np.floor(ymax + 0.5).astype('int32'))
-            xmax = min(image.size[0], np.floor(xmax + 0.5).astype('int32'))
+            ymin = max(0, np.floor(ymin + 0.5).astype("int32"))
+            xmin = max(0, np.floor(xmin + 0.5).astype("int32"))
+            ymax = min(image.size[1], np.floor(ymax + 0.5).astype("int32"))
+            xmax = min(image.size[0], np.floor(xmax + 0.5).astype("int32"))
 
             if ymin - label_size[1] >= 0:
                 text_origin = np.array([xmin, ymin - label_size[1]])
             else:
                 text_origin = np.array([xmin, ymax])
 
-
             for i in range(thickness):
                 draw.rectangle([xmin + i, ymin + i, xmax - i, ymax - i], outline=color)
-            draw.rectangle([tuple(text_origin), tuple(text_origin + label_size)], fill = color)
+            draw.rectangle(
+                [tuple(text_origin), tuple(text_origin + label_size)], fill=color
+            )
             draw.text(text_origin, thelabel, fill=(0, 0, 0), font=font)
 
     del draw
